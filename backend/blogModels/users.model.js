1cab25134275a40b59b4ecbfee69889e9231bc78
Sean Joseph <sejogoo@gmail.com>
2021-05-23 19:41:44 -0500

    Created server.js, began creating mongoose models

---
+++ backend/blogModels/users.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 5
+    },
+    password:{
+        
+    }
+})
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');  
+
+require('dotenv').config();
+
+const blogApp = express();
+const blogPort = process.env.PORT || 5000;
+
+blogApp.use(cors());
+blogApp.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
+);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+blogApp.listen(blogPort, () => {
+    console.log(`Mongo's Up on: ${blogPort}`);
+});
