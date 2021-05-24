d12703c57f5e5fdfd1d504005cd24b5002989b65
Sean Joseph <sejogoo@gmail.com>
2021-05-24 09:30:27 -0500

    Finished User Model

--- backend/blogModels/users.model.js
+++ backend/blogModels/users.model.js
@@ -11,6 +11,27 @@ const userSchema = new Schema({
         minlength: 5
     },
     password:{
-        
-    }
-})
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 5
+    },
+    userImageUrl:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 5
+    },
+    role: {type: String, required: true, trim:true},
+    email:{type:String, required: true, unique, trim:true, minlength: 5},
+    bio: String},
+    {
+        timestamps: true,
+    })
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
